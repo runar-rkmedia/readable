5313c687ffe9e055adc9f1ab6372d4da940cd5b2
Runar Kristoffersen <public@rkmedia.no>
2017-11-13 10:54:46 +0100

    Switched provided server to my fork

--- src/containers/FrontPage.tsx
+++ src/containers/FrontPage.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react'
 import Typography from 'material-ui/Typography'
+import { PostList } from 'components'
+import { APIPostI } from 'interfaces'
 
-export const FrontPage = () => (
-  <div>
-    <Typography type="display3" gutterBottom={true}>Front page</Typography>
-    <Typography type="subheading">Here there should be various sortable lists.</Typography>
-  </div>
-)
+interface Props {
+  posts: APIPostI[]
+}
+
+export const FrontPage = (props: Props) => {
+  const { posts } = props
+  return (
+    <div>
+      <Typography type="display3" gutterBottom={true}>Front page</Typography>
+      <Typography type="subheading">
+        Here there should be various sortable lists.
+      </Typography>
+      <PostList
+        showCategory={true}
+        posts={posts}
+      />
+    </div>
+  )
+}
